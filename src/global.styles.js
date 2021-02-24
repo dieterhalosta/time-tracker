e8ba78f3b4324c716ad921efc6cc9a8e9f39098a
dieterhalosta <dieter.halosta@yahoo.com>
2021-02-24 16:13:48 +0200

    started to implement the design from adobe xd to code

---
+++ src/global.styles.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+	body {
+		width: 100vw;
+		height: 100vh;
+		font-family: 'Open Sans Condensed';
+		margin: 0;
+		padding: 20px 40px;
+        color: white;
+        background-image: linear-gradient(to bottom right, #95DC66, #1A4B33);
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+		background-size: cover;
+		overflow: none;
+		@media screen and (max-width: 800px) {
+			padding: 10px;
+		}
+	}
+
+	a {
+		text-decoration: none;
+		
+	}
+
+	* {
+
+        box-sizing: border-box;
+	}
+`;
